c4a99e7178d5705e318e6274f0e482b49f51c145
Dan Galuardi <dan.galuardi@gmail.com>
2023-02-21 12:51:24 -0500

    Using Redux

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,3 +1,5 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 import { Image, Text, Platform, StyleSheet, View } from "react-native";
 import Constants from "expo-constants";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
@@ -5,6 +7,10 @@ import DirectoryScreen from "./DirectoryScreen";
 import HomeScreen from "./HomeScreen";
 import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
+import { fetchPartners } from '../features/partners/partnersSlice';
+import { fetchCampsites } from '../features/campsites/campsitesSlice';
+import { fetchPromotions } from '../features/promotions/promotionsSlice';
+import { fetchComments } from '../features/comments/commentsSlice';
 import { createStackNavigator } from "@react-navigation/stack";
 import {
     createDrawerNavigator,
@@ -139,6 +145,14 @@ const CustomDrawerContent = (props) => (
 );
 
 const Main = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+        dispatch(fetchPromotions());
+        dispatch(fetchPartners());
+        dispatch(fetchComments());
+    }, [dispatch]);
     return (
         <View
             style={{
